eaeb0a60865629ae421a237ad8cd98180cbea635
mbressel <markus.bressel@schalk-and-friends.de>
2018-11-28 19:12:51 +0100

    add status (part 3 of 4 solved)

--- app.js
+++ app.js
@@ -1,5 +1,62 @@
-const title = React.createElement(
-    'h1',
-    { id: 'main-title'},
-    'My First React Element!'
+// const title = "this is a headline";
+// const desc = "this is a paragraph";
+// const myTitleID = 'main-title';
+//
+// const header = (
+//     <header id ={ myTitleID }>
+//         <h1>{ title }</h1>
+//         <p>{ desc }</p>
+//     </header>
+// );
+
+//Component as function
+const Header = () => (
+    //If the function just returns JSX, you can delete the "return" part
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+);
+
+const Player = () => {
+    return(
+        <div className="player">
+            <span className="player-name">
+                Guil
+            </span>
+            <Counter />
+        </div>
+    );
+}
+
+
+const Counter = () => {
+    return(
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header />
+
+            {/*Players List*/}
+            <Player />
+        </div>
+    );
+}
+
+
+
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
 );
